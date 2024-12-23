7bf7d7bf6fa0af7a1ffa2608ef543b46c0c1b7f7
Sai Ashirwad <saiashirwadr@gmail.com>
2024-12-23 17:15:55 +0530

    wip

--- packages/client/src/utils/trpc.ts
+++ packages/client/src/utils/trpc.ts
@@ -1,14 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
 import type { AppRouter } from "@template/trpc";
 import { createTRPCReact, httpLink } from "@trpc/react-query";
-import superjson from "superjson";
 
-export const trpc = createTRPCReact<AppRouter>({});
+export type TRPCReact = ReturnType<typeof createTRPCReact<AppRouter>>;
+export const trpc: TRPCReact = createTRPCReact<AppRouter>({});
 
 export const trpcLinks = [
 	httpLink({
 		url: "/trpc",
-		transformer: superjson,
 	}),
 ];
 
